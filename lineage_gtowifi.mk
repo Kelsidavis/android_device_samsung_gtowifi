e2d730677be0c25621e55d1853b305dff0ca509e
Kelsi <dumbandroid@gmail.com>
2022-12-17 00:02:01 -0800

    This is a wifi only tablet

--- lineage_gtowifi.mk
+++ lineage_gtowifi.mk
@@ -1,9 +1,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from gtowifi device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
